7a1e0f06f39d0aab1d12dc9c25aeb45316ab8682
unknown <theoxgame24@gmail.com>
2023-11-30 00:29:58 +0100

    ok

--- scripts/artifacts/Garmin_Step.py
+++ scripts/artifacts/Garmin_Step.py
@@ -20,7 +20,7 @@ __artifacts_v2__ = {
             #peut avoir plusieurs paths car tuple
 
 
-import plistlib
+
 
 import plistlib
 from scripts.artifact_report import ArtifactHtmlReport
@@ -40,73 +40,73 @@ def get_garmin_step(files_found, report_folder, seeker, wrap_text, timezone_offs
     #pour chaque élément de la liste files_found, le code convertit l'élément en string
 
     for file_found in files_found:
-        file_found = str(file_found)
+            file_found = str(file_found)
         #ouvre le fichier indiqué par file_found en mode binaire (indiqué par "rb") pour la lecture.
         #Le fichier est référencé par la variable fp dans le bloc suivant
 
-        with open(file_found, "rb") as file:
+            with open(file_found, "rb") as file:
 
 
 
 
 
-            contenue = plistlib.load(file)
-            # si la clé recherchée est trouvée dans le plist (mettre la clé plist pertinente)
-            print(contenue)
-            roo = contenue['$top']['root']
-            object = contenue['$objects']
+                contenue = plistlib.load(file)
+                # si la clé recherchée est trouvée dans le plist (mettre la clé plist pertinente)
+                print(contenue)
+                roo = contenue['$top']['root']
+                object = contenue['$objects']
 
-            # Extraire la dateKey et la valueKey du noeud racine
-            date_ke = object[roo]['dateKey']
-            value_ke = object[roo]['valueKey']
+                # Extraire la dateKey et la valueKey du noeud racine
+                date_ke = object[roo]['dateKey']
+                value_ke = object[roo]['valueKey']
 
-            # Obtention de la valeur associée à dateKey
-            date_valu = object[date_ke]['NS.time']
+                # Obtention de la valeur associée à dateKey
+                date_valu = object[date_ke]['NS.time']
 
-            # Obtention des informations sur les étages
-            floors_dat = object[value_ke]
+                # Obtention des informations sur les étages
+                floors_dat = object[value_ke]
 
-            # Extraire les clés pour les étages descendus et montés
-            floors_descended_key = floors_dat['floorsDescendedKey']
-            floors_climbed_key = floors_dat['floorsClimbedKey']
+                # Extraire les clés pour les étages descendus et montés
+                floors_descended_key = floors_dat['floorsDescendedKey']
+                floors_climbed_key = floors_dat['floorsClimbedKey']
 
-            # Obtention des valeurs associées aux clés des étages
-            floors_descended = object[floors_descended_key]
-            floors_climbed = object[floors_climbed_key]
+                # Obtention des valeurs associées aux clés des étages
+                floors_descended = object[floors_descended_key]
+                floors_climbed = object[floors_climbed_key]
 
-            epoch_offset = datetime(2001, 1, 1).timestamp()
-            adjusted_timestamp = date_valu + epoch_offset
+                epoch_offset = datetime(2001, 1, 1).timestamp()
+                adjusted_timestamp = date_valu + epoch_offset
 
-            # Convertir le timestamp en objet datetime
-            date_object_utc = datetime.utcfromtimestamp(adjusted_timestamp)
+                # Convertir le timestamp en objet datetime
+                date_object_utc = datetime.utcfromtimestamp(adjusted_timestamp)
 
-            # Appliquer le fuseau horaire (par exemple, UTC+1)
-            fuseau_horaire = pytz.timezone('Europe/Paris')  # Remplacez 'Europe/Paris' par votre fuseau horaire
-            fuseau_horaire = pytz.timezone('Europe/Paris')
-            date_objec = date_object_utc.replace(tzinfo=pytz.utc).astimezone(fuseau_horaire)
+                # Appliquer le fuseau horaire (par exemple, UTC+1)
+                fuseau_horaire = pytz.timezone('Europe/Paris')  # Remplacez 'Europe/Paris' par votre fuseau horaire
+                fuseau_horaire = pytz.timezone('Europe/Paris')
+                date_objec = date_object_utc.replace(tzinfo=pytz.utc).astimezone(fuseau_horaire)
 
-            # Formater la date au format demandé
-            date_formatte = date_objec.strftime('%d.%m.%Y %H:%M:%S')
+                # Formater la date au format demandé
+                date_formatte = date_objec.strftime('%d.%m.%Y %H:%M:%S')
 
-            liste.append(('Date', date_formatte))
-            liste.append(('Floors_climbed', floors_climbed))
-            liste.append(('Floors_descended', floors_descended))
+                liste.append(('Date', date_formatte))
+                liste.append(('Floors_climbed', floors_climbed))
+                liste.append(('Floors_descended', floors_descended))
 
 
-        reports = ArtifactHtmlReport('Garmin_floors')
-        # le report folder est définit dans l'interface graphique de iLEAPP
-        reports.start_artifact_report(report_folder, 'Garmin_floors')
-        reports.add_script()
-        data_headers = ('Keys', 'Value')
-        reports.write_artifact_data_table(data_headers, liste, file_found)
+    reports = ArtifactHtmlReport('Garmin_floors')
+    # le report folder est définit dans l'interface graphique de iLEAPP
+    reports.start_artifact_report(report_folder, 'Garmin_floors')
+    reports.add_script()
+    data_headers = ('Keys', 'Value')
+    reports.write_artifact_data_table(data_headers, liste, file_found)
 
-        # génère le fichier TSV
-        tsvname = 'Garmin_floors'
-        tsv(report_folder, data_headers, liste, tsvname)
+    # génère le fichier TSV
+    tsvname = 'Garmin_floors'
+    tsv(report_folder, data_headers, liste, tsvname)
 
-        # insérer les enregistrements horodatés dans la timeline
-        # (c’est la première colonne du tableau qui sera utilisée pour horodater l’événement)
-        tlactivity = 'Garmin_floors'
-        timeline(report_folder, tlactivity, liste, data_headers)
+    # insérer les enregistrements horodatés dans la timeline
+    # (c’est la première colonne du tableau qui sera utilisée pour horodater l’événement)
+    tlactivity = 'Garmin_floors'
+    timeline(report_folder, tlactivity, liste, data_headers)
 
-        reports.end_artifact_report()
+    reports.end_artifact_report()
